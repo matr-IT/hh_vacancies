6e837f6df4b2958dbc4b29082b7d9ba12b5bac0d
rybin <rybin.32@gmail.com>
2025-09-21 14:42:37 +0300

    HH class init and small fixes

---
+++ src/HH.py
@@ -0,0 +1,38 @@
+import requests
+from abc import ABC, abstractmethod
+
+
+class HHAbstract(ABC):
+    """
+    Абстрактный класс для парсеров вакансий
+    """
+
+    def __init__(self):
+        self.vacancies = []
+
+    @abstractmethod
+    def load_vacancies(self, keyword: str):
+        """ " Получение вакансий"""
+        pass
+
+
+class HH(HHAbstract):
+    """
+    Класс для работы с API HeadHunter
+    """
+
+    def __init__(self):
+        self.url = "https://api.hh.ru/vacancies"
+        self.headers = {"User-Agent": "HH-User-Agent"}
+        self.params = {"text": "", "page": 0, "per_page": 100}
+        self.vacancies = []
+        super().__init__()
+
+    def load_vacancies(self, keyword):
+        self.params["text"] = keyword
+        while self.params.get("page") != 20:
+            response = requests.get(self.url, headers=self.headers, params=self.params)
+            vacancies = response.json()["items"]
+            self.vacancies.extend(vacancies)
+            self.params["page"] += 1
+        return self.vacancies
